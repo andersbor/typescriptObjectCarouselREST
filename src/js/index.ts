121b2d081a3af7f760f23dc9479678ea1c54045d
andersbor <anbo@easj.dk>
2018-11-19 10:41:01 +0100

    data from REST service

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,56 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+
+let uri: string = "http://jsonplaceholder.typicode.com/comments";
+
+interface IComment {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let comments: IComment[] = []; // array filled in axios.get
+let index: number = 0;
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+axios.get<IComment[]>(uri)
+    .then(function (response: AxiosResponse<IComment[]>): void {
+        comments = response.data;
+        showComment();
+    })
+    .catch(function (error: AxiosError): void {
+        console.log(JSON.stringify(error));
+    });
+
+function showComment(): void {
+    let div1: HTMLDivElement = <HTMLDivElement>document.getElementById("div1");
+    let div2: HTMLDivElement = <HTMLDivElement>document.getElementById("div2");
+    let div3: HTMLDivElement = <HTMLDivElement>document.getElementById("div3");
+    let div4: HTMLDivElement = <HTMLDivElement>document.getElementById("div4");
+    let div5: HTMLDivElement = <HTMLDivElement>document.getElementById("div5");
+    div1.innerHTML = "id: " + comments[index].id + " postId: " + comments[index].postId;
+    div3.innerHTML = "email: " + comments[index].email;
+    div4.innerHTML = "name: " + comments[index].name;
+    div5.innerHTML = comments[index].body;
+}
+
+let nextButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("nextButton");
+nextButton.addEventListener("click", nextButtonClicked);
+
+function nextButtonClicked(): void {
+    index++;
+    if (index === comments.length) { index = 0; }
+    showComment();
+}
+
+let prevButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("prevButton");
+prevButton.addEventListener("click", prevButtonClicked);
+
+function prevButtonClicked(): void {
+    index--;
+    if (index === -1) { index = comments.length - 1; }
+    showComment();
+}
